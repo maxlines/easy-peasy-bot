6eedefa01bf13b3658e7209a82a8da6554830752
Maxine Lines <maxine@Maxines-MacBook-Pro.local>
2016-08-22 15:14:12 +1000

    changes

--- index.js
+++ index.js
@@ -56,6 +56,10 @@ if (process.env.TOKEN || process.env.SLACK_TOKEN) {
     process.exit(1);
 }
 
+var apiai = require('botkit-middleware-apiai')({
+   token: process.env.APIAI_TOKEN
+});
+controller.middleware.receive.use(apiai.receive);
 
 /**
  * A demonstration for how to handle websocket events. In this case, just log when we have and have not
@@ -85,10 +89,22 @@ controller.on('bot_channel_join', function (bot, message) {
     bot.reply(message, "I'm here!")
 });
 
-controller.hears('hello', 'direct_message', function (bot, message) {
-    bot.reply(message, 'Hello!');
+/**controller.hears(['hello', 'hi', 'yo', 'good morning', 'good afternoon'], ['mention', 'direct_mention', 'direct_message'], function (bot, message) {
+    bot.reply(message, 'Hi there! :cherry_blossom: How can I help? :rabbit:');
+});
+*/
+controller.hears(['hello'], ['mention', 'direct_mention', 'direct_message'], apiai.hears, function (
+bot, message) {
+   bot.reply(message, 'Hi there! :cherry_blossom: How can I help? :rabbit:');
 });
 
+controller.hears(['book'], ['mention', 'direct_mention', 'direct_message'], apiai.hears, function (bot, message) {
+   if(message.fulfillment.speech !== '') {
+       bot.reply(message, message.fulfillment.speech);
+   } else {
+       bot.reply(message, "No problem! You want to fly to " + message.entities['geo-city'] + " on " + message.entities['date']+". :rabbit:");
+   }
+});
 
 /**
  * AN example of what could be:
